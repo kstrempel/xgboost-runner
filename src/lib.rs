ffc9b420a6f085ab9a5666719584345f619a8318
kai strempel <kai.strempel@takeaway.com>
2019-06-26 09:58:12 +0200

    fixes current warnings

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ pub struct Runner {
 }
 
 impl Runner {
-    pub fn new(model: String, features: Vec<String>) -> Runner {
+    pub fn new(_model: String, _features: Vec<String>) -> Runner {
         Runner{}
     }
 
-    pub fn predict(&self, input: Vec<f64>) -> f64 {
+    pub fn predict(&self, _input: Vec<f64>) -> f64 {
         10.1
     }
 }
